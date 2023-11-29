ad1e9bfe56062e0c0374dec27196767f9e2b01ed
samuelhackwill <hackmew@gmail.com>
2023-11-29 16:50:25 +0100

    Add globals and state management for level1 component

--- imports/ui/components/pasUnRobot.js
+++ imports/ui/components/pasUnRobot.js
@@ -1,18 +1,21 @@
 import "./pasUnRobot.html";
 
+export const PasUnRobotBeaten = new ReactiveVar(false);
+
 Template.pasUnRobot.onCreated(function () {
   this.hasInteracted = new ReactiveVar(false);
 });
 
 Template.pasUnRobot.helpers({
   hasInteracted() {
-    const instance = Template.instance();
-    return instance.hasInteracted.get();
+    return PasUnRobotBeaten.get();
   },
 });
 
 Template.pasUnRobot.events({
   "click input#pasUnRobot"() {
+    PasUnRobotBeaten.set(true);
+
     const instance = Template.instance();
     instance.hasInteracted.set(true);
   },
--- imports/ui/layouts/level_1.js
+++ imports/ui/layouts/level_1.js
@@ -1,3 +1,36 @@
 import "./level_1.html";
 import "../components/continuer.js";
 import "../components/pasUnRobot.js";
+import "../components/captcha.js";
+
+import { PasUnRobotBeaten } from "../components/pasUnRobot.js";
+import { ContinuerBeaten } from "../components/continuer.js";
+
+Template.level_1.onCreated(function () {
+  this.state = new ReactiveVar("initial");
+  // one of
+  // > initial
+  // > acteI
+  // > acteII
+});
+
+Template.level_1.onRendered(function () {
+  const instance = Template.instance();
+  Tracker.autorun(() => {
+    if (ContinuerBeaten.get() == true) {
+      instance.state.set("acteI");
+    }
+    if (PasUnRobotBeaten.get() == true) {
+      instance.state.set("acteII");
+    }
+    console.log("state of level 1 ", instance.state.get());
+  });
+});
+
+Template.level_1.helpers({
+  state(name) {
+    console.log(name);
+    const instance = Template.instance();
+    return instance.state.get() == name;
+  },
+});
